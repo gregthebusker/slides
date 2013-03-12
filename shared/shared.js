9e28ab76dc5fbbe62ed02ee6782ea8bac4d6f677
Greg Schechter <gregthebusker@gmail.com>
2013-03-12 13:35:58 -0700

    Slides Deck

---
+++ shared/shared.js
@@ -0,0 +1,32 @@
+var request = $.ajax({
+  url: "shared/slides.txt",
+}).done(function(text) {
+
+  var els = document.createElement('div');
+  els.innerHTML = text;
+
+  var shared = document.querySelectorAll('slideholder[data-shared-slides]');
+  for(var i = 0; i < shared.length; i++) {
+    var slideHolder = shared[i];
+    var ids = slideHolder.getAttribute('data-shared-slides').split(',');
+    ids.forEach(function(id) {
+      var slide = els.querySelector('#' + id);
+      slideHolder.appendChild(slide);
+    });
+  }
+
+  $('#title-h1').html($('title').html());
+
+  start();
+});
+
+
+function start() {
+  Modernizr.load({
+    test: !!document.body.classList && !!document.body.dataset,
+    nope: ['js/polyfills/classList.min.js', 'js/polyfills/dataset.min.js'],
+    complete: function() {
+      window.slidedeck = new SlideDeck();
+    }
+  });
+}
---
+++ theme/slide_config.js
@@ -0,0 +1,30 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'HTML5 at YouTube',
+    subtitle: 'Stories from the Front Line',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1',
+    favIcon: 'http://bleedinghtml5.appspot.com/images/chrome-logo-tiny2.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Greg Schechter',
+    company: 'Web Warrior, YouTube',
+    gplus: 'https://plus.google.com/u/0/109404491328800973695',
+    twitter: '@gregthebusker'
+  }, {
+    name: 'Zoltan Szego',
+    company: 'YouTube'
+  }]
+};
+
